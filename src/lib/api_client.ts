5ec33d3fc31c6dfd24cc5ae5219caeefc0673437
Lorenzo Wormer <l.wormer@pm.me>
2021-06-04 12:22:52 +0200

    removed another relic

--- src/lib/api_client.ts
+++
@@ -1,9 +0,0 @@
-import { Directus } from "@directus/sdk";
-import { ItemTypes } from "@/types/api";
-
-const apiAddress = process.browser ? process.env.NEXT_PUBLIC_CMS_URL : process.env.CMS_URL;
-if(!apiAddress) throw "Either set $NEXT_PUBLIC_CMS_URL or $CMS_URL environment variable!";
-
-const client = new Directus<ItemTypes>(apiAddress);
-
-export default client;
