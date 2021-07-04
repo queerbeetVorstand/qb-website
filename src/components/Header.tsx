808919b7b8f8b9c6246b22048eaa2a79de7ff1e7
Thomas Schmidt <thomas@thsmdt.com>
2021-07-04 12:24:19 +0200

    feat: add visual separator for header to content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,21 @@ import Box from '@material-ui/core/Box';
 import GlobalNavigation from "./GlobalNavigation";
 import NavigationItem from "./NavigationItem";
 import QBLogo from "./QBLogo";
+import { makeStyles } from "@material-ui/core";
+
+
+const borderBottomStyling = makeStyles({
+  root: {
+    borderBottom: '4pt solid',
+    borderImageSource: 'linear-gradient(to right, #FF0018, #FFA52C, #FFFF41, #008018, #0000F9, #86007D)',
+    borderImageSlice: '0% 0% 100% 0%'
+  }
+});
 
 export default function Header(): ReactElement {
+  const borderBottom = borderBottomStyling();
   return (
-    <Box display="flex" justifyContent="flex-end">
+    <Box display="flex" justifyContent="flex-end" className={borderBottom.root}>
         <Box flexGrow={1} position="relative" top={8}>
           <QBLogo />
         </Box>
