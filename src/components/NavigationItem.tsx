26526b5537d99f33763ba2152956156e75f8d918
Thomas Schmidt <thomas@thsmdt.com>
2021-07-04 02:38:29 +0200

    feat: highlight navigation items

--- src/components/NavigationItem.tsx
+++ src/components/NavigationItem.tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement } from "react";
-import { Link } from "@material-ui/core";
+import { Link, Typography } from "@material-ui/core";
 
 export default function NavigationItem(props: NavigationItemProperties): ReactElement {
   return (
-    <Link href={props.destinationUrl}>{props.children}</Link>
+    <Link href={props.destinationUrl}>
+      <Typography variant="button">{props.children}</Typography>
+    </Link>
   );
 }
 
