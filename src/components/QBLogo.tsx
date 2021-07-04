1af66f375f9bce2dde3df4f2e0371491e78cb323
Thomas Schmidt <thomas@thsmdt.com>
2021-07-04 02:39:52 +0200

    feat: add queerbeet logo

--- src/components/QBLogo.tsx
+++ src/components/QBLogo.tsx
@@ -1,10 +1,11 @@
 import React, { ReactElement } from "react";
-import { Box, Typography } from "@material-ui/core";
+import { Box } from "@material-ui/core";
+import Logo from "resources/qb_text.svg";
 
 export default function QBLogo(): ReactElement {
   return (
-    <Box m={2}>
-      <Typography color="primary">Queerbeet</Typography>
+    <Box m={1}>
+      <Logo height={50}/>
     </Box>
   );
 }
