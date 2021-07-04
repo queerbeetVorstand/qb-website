1bf72959a9e54152a1ab03ddad1e958774df3380
Lorenzo Wormer <l.wormer@pm.me>
2021-07-04 04:13:36 +0200

    feat: implemented footer with imprint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { AppProps } from "next/app";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "@/theme";
+import { Box } from "@material-ui/core";
+import Footer from "@/components/Footer";
 
 export default function MyApp(props: AppProps): ReactElement {
   const { Component, pageProps } = props;
@@ -28,7 +30,12 @@ export default function MyApp(props: AppProps): ReactElement {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
+        <Box display="flex" flexDirection="column">
+          <Box minHeight="100vh">
+            <Component {...pageProps} />
+          </Box>
+          <Footer />
+        </Box>
       </ThemeProvider>
     </React.Fragment>
   );
