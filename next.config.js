e8c3a04c43bb021f32e404567ed111753c5d82b4
Lorenzo Wormer <l.wormer@pm.me>
2021-10-05 09:42:08 +0200

    chore: remove unnecessary file-loader config

--- next.config.js
+++ next.config.js
@@ -10,17 +10,13 @@ module.exports = {
     });
 
     config.module.rules.push({
-        test: /\.(woff(2)?|ttf|eot|otf)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/'
-            }
-          }
-        ]
-    })
+      test: /\.(woff(2)?|ttf|eot|otf)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: "file-loader",
+        },
+      ],
+    });
 
     return config;
   },
