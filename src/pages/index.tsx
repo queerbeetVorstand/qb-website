f2d7850d8b0b040476c2fbfc3bf2a5b5fa0bf999
Thomas Schmidt <thomas@thsmdt.com>
2021-10-09 19:45:35 +0200

    feat: move header outside of container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,33 +7,35 @@ import Header from "@/components/Header";
 
 export default function Index(): ReactElement {
   return (
-    <Container maxWidth="md">
+    <Box>
       <Header />
-      <Box my={4}>
-        <Text variant="h5">
-          🚧 Hier entsteht gerade noch die Website der Queerbeet Hochschulgruppe
-          🚧
-        </Text>
-        <Text variant="body1">
-          Für weitere Infos zu uns und den Kontakt zu unseren Mitgliedern kannst
-          du gerne unserer{" "}
-          <Link
-            href="https://www.lists.kit.edu/sympa/subscribe/queerbeet"
-            target="_blank"
-          >
-            öffentlichen Mailingliste
-          </Link>{" "}
-          (~1 E-Mail/Woche) oder unserem
-          <Link
-            href="https://go.rocket.chat/invite?host=chat.queerbeet.org&path=invite%2F9kvNdD"
-            target="_blank"
-          >
-            {" "}
-            Rocket.Chat Server
-          </Link>{" "}
-          beitreten.
-        </Text>
-      </Box>
-    </Container>
+      <Container maxWidth="md">
+        <Box my={4}>
+          <Text variant="h5">
+            🚧 Hier entsteht gerade noch die Website der Queerbeet
+            Hochschulgruppe 🚧
+          </Text>
+          <Text variant="body1">
+            Für weitere Infos zu uns und den Kontakt zu unseren Mitgliedern
+            kannst du gerne unserer{" "}
+            <Link
+              href="https://www.lists.kit.edu/sympa/subscribe/queerbeet"
+              target="_blank"
+            >
+              öffentlichen Mailingliste
+            </Link>{" "}
+            (~1 E-Mail/Woche) oder unserem
+            <Link
+              href="https://go.rocket.chat/invite?host=chat.queerbeet.org&path=invite%2F9kvNdD"
+              target="_blank"
+            >
+              {" "}
+              Rocket.Chat Server
+            </Link>{" "}
+            beitreten.
+          </Text>
+        </Box>
+      </Container>
+    </Box>
   );
 }
